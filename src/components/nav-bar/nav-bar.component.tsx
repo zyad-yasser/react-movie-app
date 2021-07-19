20ef2634fd6bb438ae08822d154393c700fb0792
Zyad Yasser <zyad.yasser@mowesolutions.com>
2021-07-19 23:47:08 +0200

    Test intigrity of Bootstrap / Add missing styling import

--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -1,3 +1,17 @@
-import './nav-bar.component.sass'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import './nav-bar.component.sass';
 
-export const NavBar = () => (<>Navbar</>)
+export const NavBar = () => (
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  </>
+);
