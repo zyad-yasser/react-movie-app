186a810b8927d71125105cca92c4b3457b593a8d
Zyad Yasser <zyad.yasser@mowesolutions.com>
2021-07-29 01:29:42 +0200

    Add required tooltip

--- src/components/movie-header-actions/movie-header-actions.component.tsx
+++ src/components/movie-header-actions/movie-header-actions.component.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { actionButtons } from '../../statics/action-buttons';
 import { Movie } from '../../types/movies';
 import { CircleRating } from '../circle-rating/circle-rating.component';
@@ -7,24 +9,43 @@ export interface Props {
   movie: Movie;
 }
 
-export const MovieHeaderActions = ({ movie: { vote_average } }: Props) => (
-  <div className="movie-header-actions d-flex align-items-center">
-    <div className="rating-place d-flex align-items-center">
-      <div className="rating">
-        <CircleRating rating={vote_average} />
+export const MovieHeaderActions = ({ movie: { vote_average } }: Props) => {
+  return (
+    <div className="movie-header-actions d-flex align-items-center">
+      <div className="rating-place d-flex align-items-center">
+        <div className="rating">
+          <CircleRating rating={vote_average} />
+        </div>
+        <div className="caption ms-2 d-flex">User score</div>
       </div>
-      <div className="caption ms-2 d-flex">User score</div>
-    </div>
-    <div className="buttons d-flex align-items-center">
-      {actionButtons.map(({ icon }) => (
-        <button className="circle">
-          <img src={`/icons/${icon}.svg`} alt={icon} />
+      <div className="buttons d-flex align-items-center">
+        {actionButtons.map(({ icon }, key) => (
+          <OverlayTrigger
+            key={key}
+            placement={'bottom'}
+            overlay={
+              <Tooltip className="header-tooltip" id={`tooltip-${key}`}>
+                Tooltip on <strong>{icon}</strong>.
+              </Tooltip>
+            }
+          >
+            <button
+              className="circle"
+            >
+              <img src={`/icons/${icon}.svg`} alt={icon} />
+            </button>
+          </OverlayTrigger>
+        ))}
+        <button className="play d-flex align-items-center justify-content-center">
+          <img
+            className="me-1"
+            height="20px"
+            src={`/icons/play.svg`}
+            alt="play"
+          />
+          Play trailer
         </button>
-      ))}
-      <button className="play d-flex align-items-center justify-content-center">
-        <img className="me-1" height="20px" src={`/icons/play.svg`} alt="play" />
-        Play trailer
-      </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
